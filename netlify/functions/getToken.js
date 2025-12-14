4e4de38d4a1ac01aaef922fff10c13f0c8deb8e0
sahil-dev-ai <sahilnbadgewar@gmail.com>
2025-12-14 19:36:00 +0530

    Update getToken.js

--- netlify/functions/getToken.js
+++ netlify/functions/getToken.js
@@ -14,6 +14,7 @@ exports.handler = async function (event, context) {
     "https://digital-lending.figma.site",
     "https://*.figma.site",
     "https://yournaukri-hr-demo.netlify.app",
+    "https://car-insurance-app.figma.site",
     "https://szvwdt-5173.csb.app" // 👈 add your CodeSandbox URL here
   ];
 
